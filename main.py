b0abc4d1f6806ca8498d767347f4d477c176e5f0
Simon Liepold <simonliepold@gmx.de>
2026-05-05 17:14:42 +0200

    Implement path calculation enhancements: update main function to use geometry data, add PathFrenet and PathKinematics classes, and refactor exePath for improved trajectory handling. Remove deprecated geometry module and trajectory class. -- by Jan Woelke and David Pfeilschiffter --

--- main.py
+++ main.py
@@ -3,12 +3,19 @@ import path_berechnung.exe_path as exeP
 
 def main():
     print("=== Starte Pfadberechnung ===")
-    p1 = [0, 0, 0]
-    p2 = [100, 0, 0]
+    p1 = [0.4, 0.4, 0]
+    p2 = [-0.4, -0.4, 0]
     # if p1 or p2 uses image or video input
     #p1 = None
     #p2 = None
-    d = exeP.exePath().run(p1, p2, source="media/test2.png")
+    j_data = [{"type": "Line", "pts": [[0.0, 0.0, 0.0], [0.2, 0.0, 0.0]]},
+              {"type": "Arc", "c": [0.2, 0.2, 0.0], "r": 0.2, "u": [1, 0, 0],
+               "v": [0, 1, 0], "a": [-1.5708, 0.0]}, {"type": "Bezier",
+                "pts": [[0.4, 0.2, 0.0], [0.4, 0.4, 0.0], [0.2, 0.4, 0.0], [0.2, 0.4, 0.2]]},
+                {"type": "Line", "pts": [[0.2, 0.4, 0.2], [0.2, 0.4, 0.4]]}]
+
+    d = exeP.exePath().run(geometry = j_data, p1=p1, p2=p2)
+    #d = exeP.exePath().run(p1, p2, source="media/test2.png")
 
     print("\n=== Starte Motorberechnung ===")
     exeM.exeMotor().run(d)
