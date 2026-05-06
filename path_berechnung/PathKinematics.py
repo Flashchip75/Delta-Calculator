9fc74db4e2cf0d902d1ea4bbf20b6b34e019ede9
Simon Liepold <simonliepold@gmx.de>
2026-05-06 18:53:29 +0200

    Refactor initialization in PathKinematics for improved readability

--- path_berechnung/PathKinematics.py
+++ path_berechnung/PathKinematics.py
@@ -4,7 +4,9 @@ class PathKinematics:
     def __init__(self, pts, T):
         s_in = np.insert(np.cumsum(np.linalg.norm(np.diff(pts, axis=0), axis=1)), 0, 0)
         s_max = s_in[-1]; t_ref = np.linspace(0, T, 10000); tau = t_ref / T
-        self.t = interp1d(s_max * (10*tau**3 - 15*tau**4 + 6*tau**5), t_ref, fill_value="extrapolate")(s_in)
+        self.t = interp1d(s_max * (10*tau**3 - 15*tau**4 + 6*tau**5),
+                          t_ref,
+                          fill_value="extrapolate")(s_in)
         tau_v = self.t / T
         self.s = s_max * (10*tau_v**3 - 15*tau_v**4 + 6*tau_v**5)
         self.v = (s_max/T) * (30*tau_v**2 - 60*tau_v**3 + 30*tau_v**4)
