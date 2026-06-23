929bc681890aec1f9bc31901b858ad382730f913
Stefan Sedlmaier <stefleff0@gmail.com>
2026-06-23 20:57:32 +0200

    added Separator and fixed width jumping on FolderFrame

--- Oberflaeche/gui/designelemente/custom_widgets/grouping_widgets.py
+++ Oberflaeche/gui/designelemente/custom_widgets/grouping_widgets.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 
 class FolderFrame(tk.Frame):
     def __init__(self, master=None, labelText: str="Folder", isOpen: bool = True, isLocked: bool = False, **kwargs):
@@ -10,14 +11,19 @@ class FolderFrame(tk.Frame):
         self.columnconfigure(1, weight=1)
         self.rowconfigure(0, weight=0)
         self.rowconfigure(1, weight=1)
+        self.rowconfigure(2, weight=0)
 
-        self.button = tk.Button(self, width=2, command=self.toggle)
+        self.button = tk.Button(self, width=2, height=1, command=self.toggle)
         self.button.grid(row=0, column=0, sticky="nsew")
 
         self.header = tk.Label(self, text=labelText, anchor="w", font=("kDefaultFont", 10, "bold"))
         self.header.grid(row=0, column=1, sticky="nsew")
 
         self.contentFrame = tk.Frame(self)
+        self.contentFrame.grid(row=1, column=0, columnspan=2, sticky="nsew")
+
+        self.bottomSeperator = ttk.Separator(self, orient="horizontal")
+        self.bottomSeperator.grid(row=2, column=0, columnspan=2, sticky="nsew",pady=5)
 
         self.isOpen = isOpen
         if isOpen:
@@ -32,13 +38,14 @@ class FolderFrame(tk.Frame):
             self.unlock()
 
     def open(self):
-        self.contentFrame.grid(row=1, column=1, sticky="nsew")
+        self.contentFrame.grid()
+        self.contentFrame.grid_propagate(False)
         self.isOpen = True
         self.button.configure(text="\\/")
         self.button.configure(font = ("kDefaultFont", 8, "bold"))
 
     def close(self):
-        self.contentFrame.grid_forget()
+        self.contentFrame.grid_remove()
         self.isOpen = False
         self.button.configure(text=">")
         self.button.configure(font=("kDefaultFont", 8, "bold"))
