d358e5ce35865e86fdbcde6f8a7bd25d34bb4714
Flashchip75 <simonliepold@gmx.de>
2026-06-25 13:06:27 +0200

    Refactor main.py to integrate GUI application
    - Removed the existing main function that handled path and motor calculations.
    - Integrated the GUI application from Oberflaeche.gui.app.
    - Updated the entry point to launch the GUI instead of executing calculations.

--- main.py
+++ main.py
@@ -1,32 +1,5 @@
-import os
-
-from config import cfg
-import Motor_Berechnung.exe_motor as exeM
-import path_berechnung.exe_path as exeP
-
-def main():
-    print("=== Starte Pfadberechnung ===")
-    mode = "gcode"
-
-    geometry = "full_circle"
-    gcode = "test.gcode"
-    uiData = None
-
-    # --- Switch ---
-    kwargs = {
-        "geometry": dict(geometry=geometry),
-        "gcode":    dict(gcode=gcode),
-        "uiData":   dict(uiData=uiData),
-    }
-
-    g = cfg.global_cfg
-    if os.makedirs(g.output_dir, exist_ok=True):
-        print(f"Output-Verzeichnis '{g.output_dir}' erstellt.")
-
-    data = exeP.exePath().run(**kwargs[mode])
-
-    print("\n=== Starte Motorberechnung ===")
-    exeM.exeMotor().run(data=data)
+from Oberflaeche.gui.app import App
 
 if __name__ == "__main__":
-    main()
+    app = App()
+    app.mainloop()
